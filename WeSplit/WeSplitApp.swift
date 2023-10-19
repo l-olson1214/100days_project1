4bfe45034aeb5e3bbb33926dd88669780ae911a4
Grey Olson <l.olsonswift@gmail>
2023-10-19 14:48:05 -0400

    first commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Grey Olson on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
